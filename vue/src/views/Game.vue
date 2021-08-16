8886760ab1dd270d2329fcf300b1490e420a837a
pioneer4096 <y.krasnoperov@seuslab.ru>
2021-08-17 01:48:08 +0500

    заготовка для формы игры vue

---
+++ vue/src/views/Game.vue
@@ -0,0 +1,216 @@
+<template>
+  <div class="game">
+    <div style="text-align: left;">
+      <v-btn @click="cancel">Отмена</v-btn>
+
+      <p style="float:right;">4:37</p>
+    </div>
+    <br><br><br>
+    <div class="calc-btn-wrapper">
+      <v-row>
+        <v-col class="xl3">
+          <v-btn
+                  fab
+                  dark
+                  class="mx-2 calc-btn pa-2"
+                  color="pink"
+          >
+            1
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn pa-2"
+                  fab
+                  dark
+                  color="primary"
+          >
+            2
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn"
+                  fab
+                  dark
+
+                  color="primary"
+          >
+            3
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn-last"
+                  fab
+                  dark
+
+                  color="primary"
+          >
+            &lt;
+          </v-btn>
+        </v-col>
+
+      </v-row>
+
+      <v-row>
+        <v-col class="xl3">
+          <v-btn
+                  fab
+                  dark
+                  class="mx-2 calc-btn pa-2"
+                  color="pink"
+          >
+            4
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn pa-2"
+                  fab
+                  dark
+                  color="primary"
+          >
+            5
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn"
+                  fab
+                  dark
+
+                  color="primary"
+          >
+            6
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn-last"
+                  fab
+                  dark
+
+                  color="primary"
+          >
+            &gt;
+          </v-btn>
+        </v-col>
+
+      </v-row>
+
+      <v-row>
+        <v-col class="xl3">
+          <v-btn
+                  fab
+                  dark
+                  class="mx-2 calc-btn pa-2"
+                  color="pink"
+          >
+            7
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn pa-2"
+                  fab
+                  dark
+                  color="primary"
+          >
+            8
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn"
+                  fab
+                  dark
+
+                  color="primary"
+          >
+            9
+          </v-btn>
+        </v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn-last"
+                  fab
+                  dark
+
+                  color="primary"
+          >
+            ?
+          </v-btn>
+        </v-col>
+
+      </v-row>
+
+      <v-row>
+        <v-col class="xl3"></v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn pa-2"
+                  fab
+                  dark
+                  color="primary"
+          >
+            0
+          </v-btn>
+        </v-col>
+        <v-col class="xl3"></v-col>
+        <v-col class="xl3">
+          <v-btn
+                  class="mx-2 calc-btn-last"
+                  fab
+                  dark
+
+                  color="primary"
+          >
+            =
+          </v-btn>
+        </v-col>
+
+      </v-row>
+
+    </div>
+    <br><br>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Settings',
+    methods: {
+      cancel() {
+        this.$router.push('/')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .game {
+    width: 600px;
+    margin: 0 auto;
+    border: 1px solid lightgray;
+    padding: 1rem 2rem;
+  }
+
+  .calc-btn-wrapper {
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  .calc-btn {
+    background: tomato !important;
+    margin-right: 1rem;
+  }
+
+  .calc-btn-last {
+    background: #aaaaaa !important;
+  }
+
+  .btn-row {
+    margin-bottom: 2rem;
+  }
+</style>
---
+++ vue/src/views/Settings.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="settings">
+    <h1 style="text-align: left">Привет!</h1>
+    <br><br>
+
+    <div style="text-align: left;">
+      <p>Добро пожаловать на 24 тренировочный день, </p>
+      <p>Ваш последний результат - решено 10 из 25.</p>
+      <p>Общая точность 80%.</p>
+    </div>
+    <br><br><br>
+
+    <form onsubmit.prevent="" style="text-align: left;">
+      <h2>Настройки</h2>
+      <div>
+        Длительность 7 минут.
+      </div>
+
+      <div>
+        Сложность 5.
+      </div>
+
+      <ul>
+        <li>Суммирование</li>
+        <li>Разность</li>
+        <li>Умножение</li>
+        <li>Деление</li>
+        <li>Возведение в степень</li>
+      </ul>
+      <br><br>
+      <p style="text-align: right">
+        <v-btn @click="play">Играть!</v-btn>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Settings',
+  components: {},
+  methods: {
+    play() {
+      if(this.validate())
+      this.$router.push('game')
+    },
+    validate() {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .settings {
+    width: 600px;
+    margin: 0 auto;
+    border: 1px solid lightgray;
+    padding: 1rem 2rem;
+  }
+</style>
