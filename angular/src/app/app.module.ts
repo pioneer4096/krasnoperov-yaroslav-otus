02d92eca3d5def64aeb8422c6ff747d7bbd8b5eb
Ярослав Краснопёров <y.krasnoperov@seuslab.ru>
2021-09-01 20:29:27 +0500

    angular-33 создал компоненты страниц, добавил angular material

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecentlyAddedComponent } from './recently-added/recently-added.component';
+import { GoComponent } from './go/go.component';
+import { SettingsComponent } from './settings/settings.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecentlyAddedComponent,
+    GoComponent,
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
