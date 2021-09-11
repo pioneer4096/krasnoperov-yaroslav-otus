ca89f607825eaa21ad910af66ceea6dc632a79f3
Ярослав Краснопёров <y.krasnoperov@seuslab.ru>
2021-09-11 19:25:17 +0500

    заготовка форм для страниц добавленных и настроек

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'translator';
+    title = 'Translator';
+    links = [
+        {
+            href: '/',
+            text: 'Последние добавленные слова'
+        },
+        {
+            href: '/go',
+            text: 'Упражнения'
+        },
+        {
+            href: '/settings',
+            text: 'Настройки'
+        }
+    ];
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,26 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RecentlyAddedComponent } from './recently-added/recently-added.component';
-import { GoComponent } from './go/go.component';
-import { SettingsComponent } from './settings/settings.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RecentlyAddedComponent} from './recently-added/recently-added.component';
+import {GoComponent} from './go/go.component';
+import {SettingsComponent} from './settings/settings.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RecentlyAddedComponent,
-    GoComponent,
-    SettingsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RecentlyAddedComponent,
+        GoComponent,
+        SettingsComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatInputModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
