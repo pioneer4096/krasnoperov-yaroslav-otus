ab707097c0e3f9b0c345a616f4a4fcad2f0f3008
Ярослав Краснопёров <y.krasnoperov@seuslab.ru>
2021-10-11 20:39:08 +0500

    заготовка для добавления слов в список

--- angular/src/app/recently-added/recently-added.component.ts
+++ angular/src/app/recently-added/recently-added.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 
+export interface Word {
+    text: string,
+    date: number
+}
+
 @Component({
     selector: 'app-recently-added',
     templateUrl: './recently-added.component.html',
@@ -7,13 +12,15 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RecentlyAddedComponent implements OnInit {
 
-    newWord = ''
-    list = [
-        'train',
-        'bus',
-        'stone',
-        'bar'
-    ]
+    newWord:Word = {text: '', date: 0};
+
+    list:Array<Word> = [
+        {text: 'bus', date: 1633904710 * 1000},
+        {text: 'stone', date: 1633914710 * 1000},
+        {text: 'bar', date: 1633924710 * 1000},
+        {text: 'jar', date: 1633934710 * 1000},
+        {text: 'star', date: 1633944710 * 1000},
+    ].reverse()
 
     constructor() {
     }
@@ -22,10 +29,17 @@ export class RecentlyAddedComponent implements OnInit {
     }
 
     addWord() {
-        console.log('newWord = ', this.newWord)
-        if(this.newWord.length) {
-            this.list.push(this.newWord)
-            this.newWord = ''
+        if(this.newWord.text.length) {
+            this.list.unshift({
+                text: this.newWord.text,
+                date: Date.now()
+            })
+
+            this.newWord.text = ''
         }
+    }
+
+    get sublist(): Array<Word> {
+        return this.list.slice(0, 10)
     }
 }
