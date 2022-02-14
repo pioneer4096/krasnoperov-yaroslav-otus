9b65733418910fe56677c22406138bdfd92bbdd3
Ярослав Краснопёров <y.krasnoperov@seuslab.ru>
2022-02-15 00:57:41 +0500

    city add from weatherapi search, saving cities list to local storage

--- react/src/App.js
+++ react/src/App.js
@@ -1,69 +1,66 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import 'bulma/css/bulma.min.css';
-// import env from "react-dotenv";
+import env from "react-dotenv";
 import { debounce } from "lodash"
 import {Box, Card, Form} from 'react-bulma-components';
+import axios from 'axios';
+import {useLocalStorage} from './utils/use.local.storage';
 const {Control, Input, Field, Label} = Form;
 
-const autoCompletePool = ['Абакан', 'Астана', 'Белгород', 'Бердянск', 'Владимир', 'Вологда', 'Воронеж']
-
-const cityData = [
-    {
-        title: 'Пермь',
-        temperature: 30,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcMmJMRH00bF_7lmmFPQIzrIvS0fuWvxDbZxjSVTOpJGZSKCeDHof9Ie875hilO8iLfH0&usqp=CAU'
-    },
-    {
-        title: 'Красноярск',
-        temperature: 19,
-        image: 'https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png'
-    },
-]
 
 function App() {
     const [autoCompleteList, setAutoCompleteList] = useState([]);
     const [searchInput, setSearchInput] = useState('')
-    const [addedCities, setAddedCities] = useState([cityData[0], cityData[1]])
+    const [addedCities, setAddedCities] = useLocalStorage('added-cities', [])
 
-    useEffect(() => {
-        updateAutocompleteDropdown(searchInput);
-    }, [searchInput]);
-
-
-    function updateAutocompleteDropdown(search) {
-        const addedCitiesNames = addedCities.map(city => city.title)
-
-        const filteredList = search
-            ? autoCompletePool
-                .filter(cityName => Boolean((cityName.toLowerCase()).includes(search.toLowerCase())))
-                .filter(cityName => !addedCitiesNames.includes(cityName))
-            : [];
-        setAutoCompleteList(filteredList);
+    function updateAutocompleteDropdown(data) {
+        if(Array.isArray(data)) {
+            const list = data.map(d => {
+                return {
+                    id: d.id,
+                    name: d.name,
+                    fullName: `${d.name} (${d.region}, ${d.country})`
+                }
+            })
+            setAutoCompleteList(list)
+        }
+        else {
+            setAutoCompleteList([])
+        }
     }
 
     const delayedSearch = useCallback(
         debounce((value) => {
-            console.warn('debounce search = ', value)
+            axios.get(`http://api.weatherapi.com/v1/search.json?key=${env.API_KEY}&q=${value}`)
+                .then(r => updateAutocompleteDropdown(r.data))
+                .catch(e => console.warn(e))
         }, 500),
         []
     );
 
     function handleCitySearchChange(event) {
-        setSearchInput(event.target.value);
-        delayedSearch(searchInput)
+        const value = event.target.value
+        setSearchInput(value);
+        if(value) {
+            delayedSearch(value)
+        }
+        else {
+            updateAutocompleteDropdown([])
+        }
+
     }
 
-    function handleAutocompleteClick(cityName) {
-        const newCity = makeCityData(cityName)
+    function handleAutocompleteClick(cityData) {
+        const newCity = makeCityData(cityData)
         setAddedCities([...addedCities, newCity]);
         setAutoCompleteList([])
         setSearchInput('')
     }
 
-    function makeCityData(cityName) {
+    function makeCityData(city) {
         return {
-            title: cityName,
-            temperature: '',
+            ...city,
+            temperature: '+5',
             image: 'https://img.favpng.com/14/1/19/drawing-cartoon-png-favpng-LDPbKDBZUZnHi1kaG9xNwdWrA.jpg'
         }
     }
@@ -86,42 +83,49 @@ function App() {
                             autoCompleteList.length ?
                                 <div>
                                     {
-                                        autoCompleteList.map((cityName, index) => (
-                                            <div key={index} className="autocomplete-item" onClick={() => {handleAutocompleteClick(cityName)}}>{cityName}</div>
+                                        autoCompleteList.map((city, index) => (
+                                            <div key={index} className="autocomplete-item" onClick={() => handleAutocompleteClick(city)}>{city.fullName}</div>
                                         ))
                                     }
                                 </div> : ''
                         }
                     </div>
                 </Box>
+
                 <br/><br/>
+
                 <div className="added-cities-list" style={{ width: '600px', margin: '0 auto'}}>
                     <h4>Добавленные города</h4>
 
                     <div className="cities-cards" style={{display: 'flex'}}>
                         {
-                            addedCities.map((city, index) => (
-                                <Card key={'city-card-mini-' + index} className="mt-1 mr-3" style={{width: 250}}>
-                                    <Card.Content>
-                                        <img
-                                            align="right"
-                                            width="64"
-                                            height="64"
-                                            src={city.image}
-                                        />
+                            addedCities.length ?
+                                addedCities.map((city, index) => (
+                                    <Card key={'city-card-mini-' + index} className="mt-1 mr-3" style={{width: 250}}>
+                                        <Card.Content>
+                                            {
+                                                city.image &&
+                                                <img
+                                                    align="right"
+                                                    width="64"
+                                                    height="64"
+                                                    src={city.image}
+                                                />
+                                            }
 
-                                        <div className="city-name">
-                                            {city.title}
-                                        </div>
-                                        {
-                                            city.temperature &&
-                                            <div className="city-temperature">
-                                                {city.temperature}°
+                                            <div className="city-name">
+                                                {city.name}
                                             </div>
-                                        }
-                                    </Card.Content>
-                                </Card>
-                            ))
+                                            {
+                                                city.temperature &&
+                                                <div className="city-temperature">
+                                                    {city.temperature}°
+                                                </div>
+                                            }
+                                        </Card.Content>
+                                    </Card>
+                                ))
+                            : <div>нет городов</div>
                         }
                     </div>
 
