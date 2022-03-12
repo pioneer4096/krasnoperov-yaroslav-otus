3cbcda97dd799eadcf7f957546990e92a87569ec
pioneer4096 <y.krasnoperov@seuslab.ru>
2022-03-12 18:02:48 +0500

    error verifications, args parser minimist, npm run script and readme comments were added, methods were renamed

--- node/index.js
+++ node/index.js
@@ -1,57 +1,9 @@
-import { readdir } from 'fs/promises';
+import {tree} from './tree.js'
 
-async function read(path) {
-    const finalFiles = []
-    const finalDirs = []
-    const list = [path]
-
-    while(list.length) {
-        const tmpPath = list.shift()
-        const newDirs = await checkLevel(tmpPath, finalFiles, finalDirs)
-        list.unshift(...newDirs)
-    }
-
-    console.log('FILES = ', finalFiles, ' \n')
-    console.log('DIRS = ', finalDirs)
-}
-
-async function checkLevel(path, finalFiles, finalDirs) {
-    const {files, dirs} = await readPath(path)
-    finalFiles.push(...files)
-    finalDirs.push(...dirs)
-    return dirs
-}
-
-async function readPath(path) {
-    try {
-        const entities = await readdir(path, {withFileTypes: true});
-
-        const files = []
-        const dirs = []
-        for (const entity of entities) {
-            const entityPath = makePath(path, entity.name)
-            if(entity.isFile()) {
-                files.push(entityPath)
-            }
-
-            else if(entity.isDirectory()) {
-                dirs.push(entityPath)
-            }
-        }
-
-        return {
-            files,
-            dirs
-        }
-
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-function makePath(path, name) {
-    return `${path}/${name}`
-}
-
-read("../javascript")
-    .then(() => console.log('done'))
+tree()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(e => {
+        console.log(`ERROR: ${e.message}`);
+    });
